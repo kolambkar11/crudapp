e4a1fe5a46e5e6baef6949a0e90ab5f1b6e3990d
kolambkar11 <kolambkarashutosh@gmail.com>
2025-01-10 13:25:32 +0530

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,213 @@
-import Image from "next/image";
+"use client";
+import axios from "axios";
+import Header from "./components/Header";
+import { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Edit2Icon, Trash2Icon } from "lucide-react";
+
+type resultProps = {
+  id: number;
+  name: string;
+  email: string;
+  contact: string;
+};
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [id, setId] = useState(1);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [contact, setContact] = useState("");
+  const [isUpdate, setIsUpdate] = useState(false);
+  const [apiData, setapiData] = useState<resultProps[]>([]);
+  const handleAdd = async () => {
+    // add data to mockapi ap
+    try {
+      await axios.post(
+        `https://675bc38f9ce247eb19374d66.mockapi.io/nco/crudapp`,
+        {
+          name,
+          email,
+          contact,
+        }
+      );
+      setapiData((prev) => prev.filter((item) => item.id !== id));
+    } catch (error) {
+      console.log("Error ", error);
+    }
+  };
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  // getData
+  useEffect(() => {
+    const api = async () => {
+      try {
+        const response = await fetch(
+          "https://675bc38f9ce247eb19374d66.mockapi.io/nco/crudapp",
+          {
+            method: "GET",
+          }
+        );
+        if (!response.ok) {
+          throw new Error(`Error: ${response.status}`);
+        }
+        const jsonData = await response.json();
+        // console.log(jsonData);
+        setapiData(jsonData);
+        setapiData((prev) => prev.filter((item) => item.id !== id));
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+    api();
+  }, []);
+
+  const handleDelete = async (id: number): Promise<void> => {
+    if (window.confirm("Are you sure you want to delete this data?")) {
+      try {
+        await axios.delete(
+          `https://675bc38f9ce247eb19374d66.mockapi.io/nco/crudapp/${id}`
+        );
+
+        setapiData((prev) => prev.filter((item) => item.id !== id));
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+
+  const handleEdit = (id: number) => {
+    const dt = apiData.filter((item) => item.id === id);
+    console.log(typeof dt[0].id);
+    console.log(dt);
+    setId(dt[0].id);
+    setName(dt[0].name);
+    setEmail(dt[0].email);
+    setContact(dt[0].contact);
+    setIsUpdate(true);
+  };
+  const handleUpdate = async () => {
+    try {
+      const response = await axios.put(
+        `https://675bc38f9ce247eb19374d66.mockapi.io/nco/crudapp/${id}`,
+        {
+          id,
+          name,
+          contact,
+          email,
+        }
+      );
+
+      setapiData((prev) =>
+        prev.map((item) => (item.id === id ? response.data : item))
+      ); // Update UI locally
+      setIsUpdate(false);
+      setName("");
+      setContact("");
+      setEmail("");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  return (
+    <div className="p-4">
+      <Header />
+      <div className="flex gap-4 justify-around p-4 ">
+        <Input
+          placeholder="Enter Name"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+        <Input
+          placeholder="Enter Contact"
+          onChange={(e) => setContact(e.target.value)}
+          value={contact}
+        />
+        <Input
+          placeholder="Enter Email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </div>
+      <div className="flex justify-around p-4 ">
+        {isUpdate === false ? (
+          <>
+            <Button
+              type="button"
+              onClick={handleAdd}
+              className="me-3 bg-blue-400 text-dark hover:text-white"
+            >
+              Add Data
+            </Button>
+          </>
+        ) : (
+          <>
+            <Button
+              className="bg-green-400 text-black hover:text-white"
+              type="button"
+              onClick={handleUpdate}
+            >
+              Update Data
+            </Button>
+          </>
+        )}
+      </div>
+      <Table className="w-1/2 mx-auto border">
+        <TableCaption>Phone Book</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>ID</TableHead>
+            <TableHead>Name</TableHead>
+            <TableHead>Contact</TableHead>
+            <TableHead>Email</TableHead>
+            <TableHead>Action</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {apiData ? (
+            apiData.map((data) => (
+              <TableRow key={data.id}>
+                <TableCell className="text-center">{data.id}</TableCell>
+                <TableCell className="text-center">{data.name}</TableCell>
+                <TableCell className="text-center">{data.contact}</TableCell>
+                <TableCell className="text-center">{data.email}</TableCell>
+                <TableCell className="text-center">
+                  <Button
+                    className="bg-blue-400 me-3"
+                    onClick={() => {
+                      handleEdit(data.id);
+                    }}
+                  >
+                    <Edit2Icon />
+                  </Button>
+                  <Button
+                    className="bg-red-400"
+                    onClick={() => {
+                      handleDelete(data.id);
+                    }}
+                  >
+                    <Trash2Icon />
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={3} className="text-center">
+                Loading...
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
     </div>
   );
 }
